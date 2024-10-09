1a5a212fa4630daca6b93ad63960db1aefd70f73
AdrielyRodrigues <aar1@aluno.ifal.edu.br>
2024-10-09 08:29:47 -0300

    09/10/24

---
+++ [POO - Manhã][Aula 48][Revisão js][Adriely&Karol]/objetos.js
@@ -0,0 +1,27 @@
+let lanches = {
+    nome:"coxinha",
+    tipo: 'de frango',
+    foto: 'img/',
+    preco: 6.5
+};
+
+let lanches2 ={
+    nome:"pastel",
+    tipo: 'de frango',
+    foto: 'img/',
+    preco: 8.0
+};
+let lanches3 ={
+    nome:"enroladinho",
+    tipo: 'frango e queijo',
+    foto: 'img/',
+    preco: 7.5
+};
+let lanches4 ={
+    nome:"risole",
+    tipo: 'misto',
+    foto: 'img/',
+    preco: 5.5
+};
+
+
