97120ff17de5589625f8433a94c105bdea66da71
AdrielyRodrigues <aar1@aluno.ifal.edu.br>
2024-10-23 10:45:05 -0300

    23/10

--- [POO - Manhã][Aula 53][Tipos de for][Adriely&Karol]/controlador.js
+++ [POO - Manhã][Aula 53][Tipos de for][Adriely&Karol]/controlador.js
@@ -1,4 +1,25 @@
+let iptnome = document.getElementById('iptnome');
+let iptestado = document.getElementById('iptestado');
+let iptano = document.getElementById('iptano');
+let iptescudo = document.getElementById('iptescudo');
+let btnCadastrar = document.getElementById('btnCadastrar');
+
+/*----------------------------------------------------------------------------------------*/
 let containerCards = document.getElementById('containerCards');
-
-
 desenharCards(containerCards, listaDeTimes);
+
+    function quandoCadastrar(){
+        let timeTemp = {
+            nome : iptnome.value,
+            anoDeFundacao: iptano.value,
+            estadoDeOrigem:iptestado.value,
+            fotoEscudo: iptescudo.value
+            };
+
+        listaDeTimes.push(timeTemp);
+
+        desenharCards(containerCards, listaDeTimes);
+
+}
+ 
+btnCadastrar.addEventListener('click', quandoCadastrar);
