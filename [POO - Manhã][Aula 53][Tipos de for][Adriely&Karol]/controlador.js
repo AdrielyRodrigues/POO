99d1296c0d64f2719835f2169b1e8782a5b00e31
AdrielyRodrigues <aar1@aluno.ifal.edu.br>
2024-10-29 08:41:10 -0300

    29/10

--- [POO - Manhã][Aula 53][Tipos de for][Adriely&Karol]/controlador.js
+++ [POO - Manhã][Aula 53][Tipos de for][Adriely&Karol]/controlador.js
@@ -8,12 +8,32 @@ let btnCadastrar = document.getElementById('btnCadastrar');
 let containerCards = document.getElementById('containerCards');
 desenharCards(containerCards, listaDeTimes);
 
+
+    //-----------------------------------------------------------------------------------------//
+    //Código para carregar a imagem do input file.
+
+    let srcImagem;
+
+        function quandoCarregarArquivo(){
+            const fileReader = new FileReader ();
+            const arquivos = iptescudo.files;
+            
+            if(arquivos){ // Verificando se o arquivos não está vazio
+                fileReader.onload = function (event) {
+                    srcImagem = event.target.result
+                }
+                fileReader.readAsDataURL(arquivos[0]);
+            }
+        }
+        iptescudo.addEventListener('change', quandoCarregarArquivo);
+
+    //-----------------------------------------------------------------------------------------//
     function quandoCadastrar(){
         let timeTemp = {
             nome : iptnome.value,
             anoDeFundacao: iptano.value,
             estadoDeOrigem:iptestado.value,
-            fotoEscudo: iptescudo.value
+            fotoEscudo: srcImagem
             };
 
         listaDeTimes.push(timeTemp);
